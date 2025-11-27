b1c485d6dbf04af950884733e0c99d5a4503bb96
MLChinoo <mlchinoo@kazuhahub.com>
2025-11-28 01:52:58 +0800

    build: rename project

--- lllj-composite/Properties/AssemblyInfo.cs
+++ lllj-composite/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("atri-composite")]
+[assembly: AssemblyTitle("lllj-composite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("atri-composite")]
+[assembly: AssemblyProduct("lllj-composite")]
 [assembly: AssemblyCopyright("Copyright © lictex_")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Forked by MLChinoo")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
